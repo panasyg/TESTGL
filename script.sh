abc7466c3a55fe8cd453c047508169cedec6f0bf
Johnny <91308486+panasyg@users.noreply.github.com>
2022-11-08 19:38:33 +0200

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#installing apache webserver
+sudo yum install httpd -y
+
+#opening 80 http port in firewall
+sudo systemctl enable firewalld
+sudo systemctl start firewalld
+sudo firewall-cmd --permanent --add-service=http
+sudo firewall-cmd --reload
+
+#starting apache
+sudo systemctl start httpd
+
+#changing apache webpage content
+sudo chown -R $USER:$USER /usr/share/httpd/noindex/
+sudo echo Ivan Panas > /usr/share/httpd/noindex/index.html
